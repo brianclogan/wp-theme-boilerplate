92826be842e69ed6334c1338a7184027c40fc299
Brian Logan <brianldj@gmail.com>
2022-03-10 13:03:41 -0700

    version bump

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ define('WP_THEME_BOILERPLATE_GITHUB_REPO', 'brianclogan/wp-theme-boilerplate');
 define('WP_THEME_BOILERPLATE_GITHUB_REPO_URL', 'https://github.com/brianclogan/wp-theme-boilerplate');
 define('WP_THEME_BOILERPLATE_SLUG', 'wp-theme-boilerplate');
 define('WP_THEME_BOILERPLATE_NAME', 'WP Theme Boilerplate');
-define('WP_THEME_BOILERPLATE_VERSION', '0.3.0');
+define('WP_THEME_BOILERPLATE_VERSION', '0.3.1');
 
 /**
  * Setup the theme.
